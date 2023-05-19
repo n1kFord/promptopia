9d56f0ed0fd970805cc8bf2c6deb944dd804efb8
Nikita <nikita@MacBook-Pro-Nikita.local>
2023-05-19 19:13:39 +0600

    change form placeholder for tag

--- components/Form.jsx
+++ components/Form.jsx
@@ -43,7 +43,7 @@ const Form = ({ type, post, setPost, submitting, handleSubmit }) => {
                         onChange={(e) =>
                             setPost({ ...post, tag: e.target.value })
                         }
-                        placeholder="#tag"
+                        placeholder="tag"
                         required
                         className="form_input"
                     />
